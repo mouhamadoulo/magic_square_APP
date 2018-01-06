83866432089921fff023e202a3215d27c2077efb
Moustapha_LO <mouhamadoulo39@gmail.com>
2018-01-06 16:40:24 +0100

    Ajout de la seconde Activité et de son layout

--- app/src/main/java/com/example/ibrahim/game_app/MainActivity.java
+++ app/src/main/java/com/example/ibrahim/game_app/MainActivity.java
@@ -1,13 +1,38 @@
 package com.example.ibrahim.game_app;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btn_start;
+    Button btn_exit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //récupération des composants de notre GUI
+        btn_start = (Button) findViewById(R.id.btnSTART);
+        btn_exit = (Button) findViewById(R.id.btnEXIT);
+
+        //ajout d'événements sur nos composants
+        btn_start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent myintent = new Intent(getApplicationContext(),GameActivity.class);
+                startActivity(myintent);
+            }
+        });
+        btn_exit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
